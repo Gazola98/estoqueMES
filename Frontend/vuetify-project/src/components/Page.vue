d744aefd79c36e7e6605038d1f42a0420ffb04f9
Pereira <b1400220@Hyundai-Brasil.com>
2025-03-21 23:01:40 -0300

    nothing

--- Frontend/vuetify-project/src/components/Page.vue
+++ Frontend/vuetify-project/src/components/Page.vue
@@ -36,92 +36,6 @@
       </v-app-bar>
   
       <v-main>
-        <v-container>
-          <h1 class="ma-4">Teste</h1>
-
-          <v-card flat class="border mb-4">
-            <div class="d-flex justify-space-between">
-
-              <v-card-title class="ma-2">All Devices</v-card-title>
-
-              <v-card-title>
-                <v-btn @click ='isDialogOpen = true' variant="elevated" color="indigo-darken-4" class="mb-4">Add Device</v-btn>
-
-                <v-dialog v-model="isDialogOpen" width="600px">
-                  <v-card>
-                    <v-card-title class="mt-4">Register New Device</v-card-title>
-                    <v-card-text>
-                      <v-row>
-                        <v-col>
-                          <v-text-field label="NameDevice" variant="outlined"
-                          :rules="nameRules"></v-text-field>
-                        </v-col>
-                        
-                        <v-col>
-                          <v-select variant="outlined" label="System" :items="['MES']"></v-select>
-                        </v-col>
-                      </v-row>
-                      
-                      <v-select label="Shop"
-                      variant="outlined"
-                      :items="['ASSEMBLY', 'PAINT', 'BODY', 'PRESS', 'ENGINE']"></v-select>
-                    </v-card-text>
-
-                    <v-card-actions>
-                      <v-btn variant="text" @click="isDialogOpen = false">Cancel</v-btn>
-                      <v-btn variant="elevated" color="indigo-darken-4" class="ma-4">Salvar</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </v-card-title>
-            </div>
-          
-            <v-table>
-              <thead>
-                <tr class="text-h6">
-                  <th>Device</th>
-                  <th>Amount</th>
-                  <th>In Line</th>
-                  <th>Stock</th>
-                  <th>Shop</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>MESPC</td>
-                  <td>18</td>
-                  <td>14</td>
-                  <td>4</td>
-                  <th>ASSEMBLY</th>
-                  <td>
-                    <v-btn variant="tonal">Edit</v-btn>
-                  </td>
-                </tr>
-                <tr>
-                  <td>GPTIS</td>
-                  <td>12</td>
-                  <td>8</td>
-                  <td>4</td>
-                  <th>ASSEMBLY</th>
-                  <td>
-                    <v-btn variant="tonal">Edit</v-btn>
-                  </td>
-                </tr>
-                <tr>
-                  <td>OS&D</td>                  
-                  <td>10</td>                  
-                  <td>7</td>                  
-                  <td>3</td>
-                  <th>ASSEMBLY</th>     
-                  <td>
-                    <v-btn variant="tonal">Edit</v-btn>
-                  </td>             
-                </tr>
-              </tbody>
-            </v-table>
-          </v-card>
-        </v-container>
         
       </v-main>
     </v-app>
@@ -161,6 +75,27 @@ const toggleTheme = () => {
     theme.global.name.value = 'light'; // tema claro
   }
 };
+
+const search = ref('')
+const items = [
+  {
+    name: 'OSD',
+    image: '1.png',
+  }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
 </script>
 
 <style>
@@ -174,3 +109,81 @@ const toggleTheme = () => {
     height: 0;
   }
 </style>
+
+
+
+<!-- <v-table>
+  <thead>
+    <tr class="text-h6">
+      <th>Device</th>
+      <th>Amount</th>
+      <th>In Line</th>
+      <th>Stock</th>
+      <th>Shop</th>
+      <th>Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>MESPC</td>
+      <td>18</td>
+      <td>14</td>
+      <td>4</td>
+      <th>ASSEMBLY</th>
+      <td>
+        <v-btn variant="tonal">Edit</v-btn>
+      </td>
+    </tr>
+    <tr>
+      <td>GPTIS</td>
+      <td>12</td>
+      <td>8</td>
+      <td>4</td>
+      <th>ASSEMBLY</th>
+      <td>
+        <v-btn variant="tonal">Edit</v-btn>
+      </td>
+    </tr>
+    <tr>
+      <td>OS&D</td>                  
+      <td>10</td>                  
+      <td>7</td>                  
+      <td>3</td>
+      <th>ASSEMBLY</th>     
+      <td>
+        <v-btn variant="tonal">Edit</v-btn>
+      </td>             
+    </tr>
+  </tbody>
+</v-table> -->
+
+<!-- <v-card-title>
+  <v-btn @click ='isDialogOpen = true' variant="elevated" color="indigo-darken-4" class="mb-4">Add Device</v-btn>
+
+  <v-dialog v-model="isDialogOpen" width="600px">
+    <v-card>
+      <v-card-title class="mt-4">Register New Device</v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col>
+            <v-text-field label="NameDevice" variant="outlined"
+            :rules="nameRules"></v-text-field>
+          </v-col>
+          
+          <v-col>
+            <v-select variant="outlined" label="System" :items="['MES']"></v-select>
+          </v-col>
+        </v-row>
+        
+        <v-select label="Shop"
+        variant="outlined"
+        :items="['ASSEMBLY', 'PAINT', 'BODY', 'PRESS', 'ENGINE']"></v-select>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn variant="text" @click="isDialogOpen = false">Cancel</v-btn>
+        <v-btn variant="elevated" color="indigo-darken-4" class="ma-4">Salvar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</v-card-title> -->
